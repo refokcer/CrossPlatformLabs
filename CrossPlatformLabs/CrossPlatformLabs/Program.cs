e03d0dd7acbe2744f2f636212267b0bf65905254
refokcer <refokcer@gmail.com>
2024-09-21 20:22:01 +0300

    Solution Created

---
+++ CrossPlatformLabs/CrossPlatformLabs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrossPlatformLabs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
